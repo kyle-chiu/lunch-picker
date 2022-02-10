84f90c50297cec7ce17b240f295afb031e06b440
Kyle Chiu <kchiu132@gmail.com>
2022-02-09 22:49:05 -0700

    feat: retrieve metadata from org

---
+++ force-app/main/default/aura/LunchPicker2/LunchPicker2Controller.js
@@ -0,0 +1,15 @@
+({
+    init: function(component){
+        var columns = [
+            { label: 'Restaurant Name', fieldName: 'Name' },
+            { label: 'Wait Time', fieldName: 'Wait_Time__c', type: 'url' },
+            { label: 'Price', fieldName: 'Price__c'}
+        ];
+        var action = component.get("c.queryRestaurants");
+        action.setCallback(this, function(a){
+            component.set("v.restaurant", a.getReturnValue());
+        });
+        $A.enqueueAction(action);
+    }
+
+})
---
+++ force-app/main/default/aura/LunchPickerApp/LunchPickerApp.app
@@ -0,0 +1,3 @@
+<aura:application >
+    <c:LunchPicker2/>
+</aura:application>
